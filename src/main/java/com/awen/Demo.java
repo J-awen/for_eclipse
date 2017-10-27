0dd3e33fa0a8ea8926edb8129c6aa325a45155ff
J-awen <wenping411@qq.com>
2017-10-27 16:19:36 +0800

    测试

--- src/main/java/com/awen/Demo.java
+++ src/main/java/com/awen/Demo.java
@@ -3,6 +3,13 @@ package com.awen;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.junit.Test;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.awen.util.DomainFactory;
+
 public class Demo {
 
 	public static void main(String[] args) {
@@ -10,5 +17,19 @@ public class Demo {
 		Pattern pattern = Pattern.compile("^a[abc]*c$");
 		Matcher matcher = pattern.matcher("abbbbc");
 		boolean result = matcher.find();
+	}
+
+	@Test
+	public void test1() {
+		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+		DomainFactory obj = (DomainFactory) factory.getBean("domainFactory");
+		obj.getMessage();
+	}
+
+	@Test
+	public void test2() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		DomainFactory obj = (DomainFactory) context.getBean("domainFactory");
+		obj.getMessage();
 	}
 }
