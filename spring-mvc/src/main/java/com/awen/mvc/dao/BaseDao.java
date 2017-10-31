3ab449dcd7a6bec338079e763e8d2370d2a08cb3
awenstyle <wenping411@qq.com>
2017-10-31 15:51:04 +0800

    11

--- spring-mvc/src/main/java/com/awen/mvc/dao/BaseDao.java
+++ spring-mvc/src/main/java/com/awen/mvc/dao/BaseDao.java
@@ -6,9 +6,16 @@ import javax.sql.DataSource;
 
 public interface BaseDao<T> {
 	void setDataSource(DataSource dataSource);
+
 	List<T> find();
+
+	List<T> find(T t);
+
 	T get(Long id);
+
 	void delete(Long id);
+
 	void update(Long id);
+
 	void insert(T t);
 }
