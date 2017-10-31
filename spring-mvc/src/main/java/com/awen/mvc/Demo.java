a5d66258e86c38cc7b8b115578eb3c8a57042f51
J-awen <wenping411@qq.com>
2017-10-31 11:11:01 +0800

    11

--- spring-mvc/src/main/java/com/awen/mvc/Demo.java
+++ spring-mvc/src/main/java/com/awen/mvc/Demo.java
@@ -4,12 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
-import com.awen.jta.TestJta;
 import com.awen.mvc.util.DomainFactory;
 
 public class Demo {
